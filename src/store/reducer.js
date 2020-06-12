32d0269a0588de39107755c70e415eb7d28db74e
Sebastian Baez <sdbb.unl@gmail.com>
2020-06-12 12:54:43 +0200

    First Commit

---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+import { combineReducers } from "redux";
+import auth from "./auth/reducer";
+import orders from "./orders/reducer";
+import products from "./products/reducer";
+import user from "./user/reducer";
+const reducer = combineReducers({
+  auth: auth,
+  orders: orders,
+  products: products,
+  user: user,
+});
+
+export default reducer;
